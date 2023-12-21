79d55af98eea494225cdc94a13e33788143e61d4
surface74 <surface74@example.com>
2023-12-22 00:30:27 +0300

    feat: add headers

--- server.js
+++ server.js
@@ -1,25 +1,52 @@
-const express = require("express");
-const request = require("request");
+const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer')
+const request = require('request');
 
-require("dotenv").config();
+const upload = multer();
+
+require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3001;
-const token = process.env.TOKEN || '???';
+const port = process.env.PORT || 8080;
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.ORIGIN || "*");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Accept');
   next();
 });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  const symbol = req.query["symbol"];
-  const range = req.query["range"];
-  const url = `https://cloud.iexapis.com/stable/stock/${symbol}/batch?types=quote,chart&range=${range}&token=${token}`;
-console.log(url);
-  request(url).pipe(res);
+
+app.get('/', (req, res) => {
+  console.log('[GET] root');
+  res.send({ 'msg': 'Welcome to CORS proxy-server 😁' })
+});
+
+app.post('/proxy', upload.array(), (req, res, next) => {
+  console.log('[POST] /proxy');
+  const { endpoint, query } = req.body;
+  // const endpoint = 'https://graphql-pokemon2.vercel.app';
+  // const query = `{
+  //   pokemons(first: 1) {
+  //     id
+  //     name
+  //   }
+  //   pokemon(id: "UG9rZW1vbjowMDE=") {
+  //     name
+  //   }
+  // }`;
+
+  request.post(endpoint,
+    {
+      body: JSON.stringify({query}),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }).pipe(res);
 });
 
 app.listen(port, () =>
-  console.log(`StockChart app listening on port ${port}!`)
+  console.log(`Proxy listening on port ${port}!`)
 );
